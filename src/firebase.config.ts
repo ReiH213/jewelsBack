76605f181d3f6e14f23262ef3ed94b19e741597b
Rei <rei.haxhiraj@xyzteknoloji.com>
2024-12-09 06:35:26 +0300

    First Commit

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,11 +1,10 @@
 import * as admin from 'firebase-admin';
 
+const serviceAccount = require('../firebase.json');
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export const firestore = admin.firestore();
+export const auth = admin.auth(); // Access Authentication
